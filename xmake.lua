9c1203c1fbc79212706a5bca50ce91246e774e73
ShifftC <shifftc@gmail.com>
2024-09-24 03:15:32 +0200

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,6 @@
 -- Copyright 2024 ShifftC. Licensed under the Apache License, Version 2.0.
 
-add_requires("imgui docking", "okraylib master")
+add_requires("imgui docking", "ok-raylib master")
 
 set_project("rlImGui")
 set_version("0.0.1")
@@ -11,7 +11,7 @@ target("rlImGui", function()
     set_default(false)
     set_kind("static")
 
-    add_packages("imgui", "okraylib", {public = true})
+    add_packages("imgui", "ok-raylib", {public = true})
 
     add_files("rlImGui.cpp")
     add_headerfiles("rlImGui.h", "rlImGuiColors.h", "imgui_impl_raylib.h", "extras/*.h")
